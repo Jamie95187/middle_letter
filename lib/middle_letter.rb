0db0ee175d218d10a9e3c3804df4bd2642a16a5e
Jamie95187 <jamiewong63@googlemail.com>
2019-12-06 11:07:38 +0000

    add first test and add middle_letter.rb

---
+++ lib/middle_letter.rb
@@ -0,0 +1,9 @@
+# Your job is to return the middle character of a word.
+# If the word's length is odd, return the middle character. If the word's
+# length is even, return the middle 2 characters.
+
+class MiddleLetter
+  def middle_letter(string)
+    string
+  end
+end
---
+++ spec/middle_letter_spec.rb
@@ -0,0 +1,17 @@
+require 'middle_letter'
+
+describe MiddleLetter do
+  it "should return 'A'" do
+    expect(subject.middle_letter("A")).to eq("A")
+  end
+
+  it "should return 'B'" do
+    expect(subject.middle_letter("B")).to eq("B")
+  end
+end
+
+# get_middle("test") # => "es"
+# get_middle("testing") # => "t"
+# get_middle("middle") # => "dd"
+# get_middle("A") # => "A"
+# get_middle("of") # => "of"
